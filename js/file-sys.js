f29bf2bb3511000c420b817b45ea4c79e7d258f1
heckadecimal <heckadec@gmail.com>
2026-07-08 22:36:39 -0400

    js setup

---
+++ js/file-sys.js
@@ -0,0 +1,13 @@
+// FILE:  -->
+// FOLDER:  -->
+
+// └─       ├─
+
+let folderspan = `<span class="nerd"></span>`;
+let filespan = `<span class="nerd"></span>`;
+
+let folders = document.querySelectorAll(".folder");
+
+let files = document.querySelectorAll(".file");
+
+console.log(folders.length);
