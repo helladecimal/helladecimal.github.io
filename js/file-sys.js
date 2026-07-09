ec2d8b6e9ac9859ea3779edcfe7b50c96f817c6a
heckadecimal <heckadec@gmail.com>
2026-07-09 00:16:00 -0400

    file sys js wip

--- js/file-sys.js
+++ js/file-sys.js
@@ -1,13 +1,38 @@
 // FILE:  -->
 // FOLDER:  -->
 
-// └─       ├─
+// └─       ├─      │
 
-let folderspan = `<span class="nerd"></span>`;
-let filespan = `<span class="nerd"></span>`;
+let folderIcon = document.createElement("span");
+folderIcon.innerHTML = "";
+folderIcon.className = "nerd";
+
+let fileIcon = document.createElement("span");
+fileIcon.innerHTML = "";
+fileIcon.className = "nerd";
+
+let endBracket = document.createElement("span");
+endBracket.innerHTML = "└─ ";
+endBracket.className = "bracket";
+
+let bracket = document.createElement("span");
+bracket.innerHTML = "├─ ";
+bracket.className = "bracket";
 
 let folders = document.querySelectorAll(".folder");
 
 let files = document.querySelectorAll(".file");
 
-console.log(folders.length);
+folders.forEach(function(folder){
+    folder.prepend(folderIcon.cloneNode(true));
+});
+
+files.forEach(function(file){
+    file.prepend(fileIcon.cloneNode(true));
+
+    if (file.nextSibling != null){
+        file.prepend(bracket.cloneNode(true));
+    } else {
+        file.prepend(endBracket.cloneNode(true));
+    }
+});
