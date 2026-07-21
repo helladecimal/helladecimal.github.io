756f03c0b2211ecc477acdcf93592167949f9d92
heckadecimal <heckadec@gmail.com>
2026-07-20 23:35:54 -0400

    more stuff

--- build.c
+++ build.c
@@ -2,19 +2,21 @@
 #include <string.h>
 #include <stdbool.h>
 
+struct Identifier {
+    char* md; // Markdown identifier
+    char* htmlStart; // Html starting element
+    char* htmlEnd; // Html closing element
+    bool mdSpans; // Does the identifier span the text?
+};
+
+void get_buffer_from_ident(struct Identifier* id, char* buffer);
+
 int main(void)
 {
     FILE *test;
     char buffer[256];
 
-    struct Identifier {
-        char* md; // Markdown identifier
-        char* htmlStart; // Html starting element
-        char* htmlEnd; // Html closing element
-        bool mdSpans; // Does the identifier span the text?
-    };
-
-    struct Identifier list[] = {
+        struct Identifier list[] = {
         {"# ", "<h1>", "</h1>", false},
         {"## ", "<h2>", "</h2>", false},
         {"### ", "<h3>", "</h3>", false},
@@ -35,11 +37,40 @@ int main(void)
 
     test = fopen("blog/md/test.md", "r");
 
+    char testbuff[10];
+
     while (fgets(buffer, 256, test)){
         for (int i=0; i<( sizeof(list) / sizeof(list[1]) ); i++){
-            
+            get_buffer_from_ident(&list[i], buffer);
         }
-        printf("%s", buffer);
+        //printf("%s", buffer);
     }
     fclose(test);
+    return 0;
+}
+
+void get_buffer_from_ident(struct Identifier* id, char* buffer){
+    //printf("%s\n", buffer);
+    char new[256];
+
+    int i = 0;
+    int b = 0;
+    while (buffer[i] != '\0'){ 
+        int md_len = strlen(id->md);
+        bool matches = true;
+
+        for (int ii=0; ii<md_len; ii++){
+            if (id->md[ii] != buffer[ii]) {matches = false;}
+        }
+
+
+        if (matches == true && i > md_len-1){
+            new[b] = buffer[i];
+            new[b+1] = '\0';
+            b++;
+        }
+        i++;
+    }
+
+    printf("%s : %s\n", id->md, new);
 }
